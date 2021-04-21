a0b74e2726fcaa54e6d2823328e4634f3effc275
Yefry Figueroa <yefryfigueroa122@gmail.com>
2021-04-21 20:37:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,13 @@ import sys
 
 from flask import Flask
 
-
 app = Flask(__name__)
 
 HELLO_WORLD = 'Hello World'
 
-
 @app.route('/', methods=['GET'])
 def hello_world():
     return HELLO_WORLD
-
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
